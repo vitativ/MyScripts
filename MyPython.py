c3ef546070e2f4a06296f46664e74ea845b245a8
vitativ <vitativ@users.noreply.github.com>
2016-07-03 16:32:57 +0300

    scp python script

---
+++ MyPython.py
@@ -0,0 +1,19 @@
+import sys
+import os
+
+def main():
+	source = 'peselv@' + sys.argv[1]
+	target = sys.argv[2]
+    run('scp -q %s %s' % (source,target))
+	
+def run(cmd):
+    print cmd
+    os.system(cmd)
+
+if __name__ == '__main__':
+    main()
+	
+#scp peselv@BuildNode:/home/peselv/workspace/Build-CourseProject/pom.xml /home/peselv
+#python MyPython.py BuildNode:/home/peselv/workspace/Build-CourseProject/target/courseProject.war /home/peselv
+
+#/opt/tomcat/webapps
